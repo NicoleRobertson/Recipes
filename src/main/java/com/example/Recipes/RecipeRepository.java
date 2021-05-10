c48946ad6edf4a25033e8cccfc03de83d308f282
Rebecca Manhem <rebecca.manhem@gmail.com>
2021-05-10 09:40:08 +0200

    Created RecipeRepository

---
+++ src/main/java/com/example/Recipes/RecipeRepository.java
@@ -0,0 +1,17 @@
+package com.example.Recipes;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RecipeRepository {
+    private List<Recipe> recipes;
+
+    public RecipeRepository() {
+        recipes = new ArrayList<>();
+
+
+    }
+}
