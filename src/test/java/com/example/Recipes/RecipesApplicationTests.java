5468aa9a5900d9b47e9541339b2bb0c73010bceb
Nicole Omadova Robertson <nicole.omadovarobertson@hm.com>
2021-05-11 10:25:13 +0200

    Finalized Unit Test

--- src/test/java/com/example/Recipes/RecipesApplicationTests.java
+++ src/test/java/com/example/Recipes/RecipesApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.Recipes;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+
 
 
 @SpringBootTest
@@ -14,8 +15,12 @@ class RecipesApplicationTests {
 
 	@Test
 	public void getRecipeTest() {
-		Recipe recipe = repository.getRecipe(9L);
-		Assert.hasText("Blabla", recipe.getName());
+		Recipe recipe = repository.getRecipe(1L);
+		Assertions.assertEquals("Pasta Carbonara", recipe.getName());
+		Assertions.assertTrue(recipe.isVegetarian());//vegeterian test
+		Assertions.assertFalse(recipe.isVegetarian());//non-vegeterian test
+		Assertions.assertEquals(Cuisine.ITALIAN, recipe.getCuisine());
+
 	}
 
 }
